21005b2816e5a41a1b9d5535f54ed7b0afc41288
AhemdSaeed <ahmed.m.saeed120@gmail.com>
2025-10-23 17:44:48 +0300

    edit

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 import mysql.connector
-from mysql.connector import Error
 
 try:
     # الاتصال بالسيرفر
@@ -12,15 +11,14 @@ try:
 
     if connection.is_connected():
         cursor = connection.cursor()
-        # إنشاء قاعدة البيانات (لو مش موجودة)
+        # إنشاء قاعدة البيانات لو مش موجودة
         cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
         print("Database 'alx_book_store' created successfully!")
 
-except Error as e:
+except mysql.connector.Error as e:
     print(f"Error while connecting to MySQL: {e}")
 
 finally:
-    # إغلاق الاتصال
     if connection.is_connected():
         cursor.close()
         connection.close()
